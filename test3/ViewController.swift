b92696d35c5a855a1b4f122ba0b0933f0055d946
Oleg Lysenko <o.lysenko@itnova.pro>
2017-01-29 20:37:09 +0300

    Initial Commit

---
+++ test3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test3
+//
+//  Created by Олег Лысенко on 29.01.17.
+//  Copyright © 2017 Oleg Lysenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
